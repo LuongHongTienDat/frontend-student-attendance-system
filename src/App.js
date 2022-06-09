6687aa0b2c3275594e0925a894785dabd0d63682
ntnghia2002 <nghia.nguyen26402@gmail.com>
2022-06-09 18:06:39 +0700

    push form register

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import {Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Home from './Pages/Home/Home';
 import Layout from './Layouts/Layout';
-
+import Login from './Pages/Login/Login';
+import Form from './Components/Form';
 
 function App() {
   return (
-    <Layout>
+    
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/form-register" element={<Form/>}/>
+            <Route path="/form-login" element={<Login/>}/>
         </Routes>
-    </Layout>
+   
   );
 }
 export default App;

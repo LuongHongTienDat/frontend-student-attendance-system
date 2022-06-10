453178908b158831e013077689df3d5e8a86ff86
LuongHongTienDat <71415613+LuongHongTienDat@users.noreply.github.com>
2022-06-10 18:56:15 +0700

    update home

--- src/Layouts/Layout.js
+++ src/Layouts/Layout.js
@@ -1,7 +1,7 @@
 
 // i don't know why but dùng header nào thì import header đó thôi 
 // và comment cái (header) kia, vì khi import cùng lúc 2 header tui thấy bị lỗi
-
+import {Fragment} from 'react'
 import Header from './Header'
 // import Header_0 from './header_0/Header_0'
 
@@ -9,11 +9,11 @@ import Footer from './Footer'
 
 const Layout = (props) => {
     return (
-        <div>
+        <Fragment>
             <Header />
                 {props.children}
             <Footer />
-        </div>
+        </Fragment>
     )
 }
 export default Layout;

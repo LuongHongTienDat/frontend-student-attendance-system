8ea2015f7dde659a77a47f7839160de0aa251f1a
ntnghia2002 <nghia.nguyen26402@gmail.com>
2022-06-10 20:44:04 +0700

    1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Home from './Pages/Home/Home';
 import Layout from './Layouts/Layout';
-<<<<<<< HEAD
 import Login from './Pages/Login/Login';
 import Form from './Components/Form';
 
@@ -16,19 +15,6 @@ function App() {
             <Route path="/form-login" element={<Login/>}/>
         </Routes>
    
-=======
-import Register from './Pages/Register/Register';
-import Login from './Pages/Login/Login';
-
-function App() {
-  return (
-  
-        <Routes>
-            <Route path="/" element={<Layout/>}/>
-            <Route path="/form-register" element={<Register/>}/>
-            <Route path="/form-login" element={<Login/>}/>
-        </Routes>
->>>>>>> 74cce0d9a83038bc2d6b35ae105166789dd0baad
   );
 }
 export default App;

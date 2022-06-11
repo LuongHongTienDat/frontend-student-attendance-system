10ac47e442f9d5a3952ac2130427a5ac9fcf851e
ntnghia2002 <nghia.nguyen26402@gmail.com>
2022-06-11 11:19:46 +0700

    fix css module

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -5,73 +5,73 @@ import Footer from "../../Layouts/Footer";
 import useForm from '../../Components/useForm';
 import validate from '../../Components/ValidateForm';
 // import validate from '../../Components/ValidateForm';
-import './Register.css'
+import styles from './Register.module.css'
 
 
-const Register = (submitForm) => {
+const Register = () => {
     const {handleChange, values, handleSubmit,errors} = useForm(validate)
     
     return (
-        <div className="container">
+        <div className={styles.container}>
         <Header></Header>   
-        <div className='form-register'>
-        <form action="" method="POST" className="form" id="form-1" onSubmit={handleSubmit}>
-        <h3 className="heading">Đăng ký tài khoản</h3>
+        <div className={styles.formRegister}>
+        <form action="" method="POST" className={styles.form} id="form-1" onSubmit={handleSubmit}>
+        <h3 className={styles.heading}>Đăng ký tài khoản</h3>
        
-        <div className="form-group">
-            <label htmlFor="fullname" className="form-label">Tên đầy đủ</label>
-            <input id="fullname" type="text" name="fullname" placeholder="Nguyễn Văn A" className="form-input" value={values.fullname} onChange = {handleChange}
+        <div className={styles.formGroup}>
+            <label htmlFor="fullname" className={styles.formLabel}>Tên đầy đủ</label>
+            <input id="fullname" type="text" name="fullname" placeholder="Nguyễn Văn A" className={styles.formInput} value={values.fullname} onChange = {handleChange}
             />
-            {errors.fullname &&  <span className="form-message">{errors.fullname}</span>}
+            {errors.fullname &&  <span className={styles.formMessage}>{errors.fullname}</span>}
         </div>
         
-        <div className="form-group">
-        <div className="form-group-2">
-            <label  htmlFor="id" className="form-label">Mã số sinh viên</label>
+        <div className={styles.formGroup}>
+        <div className={styles.formGroup2}>
+            <label  htmlFor="id" className={styles.formLabel}>Mã số sinh viên</label>
             <div> 
-                <input id="id" type="text" name="id" placeholder="2010448" className="form-input " value={values.id} onChange = {handleChange}/>
+                <input id="id" type="text" name="id" placeholder="2010448" className={styles.formInput}  value={values.id} onChange = {handleChange}/>
                 <span>  </span>
-                {errors.id &&  <span className="form-message">{errors.id}</span>}        
+                {errors.id &&  <span className={styles.formMessage}>{errors.id}</span>}        
             </div>
         </div>
             
-        <div className="form-group-2 form-date">
-            <label  htmlFor="birthday" className="form-label">Ngày sinh</label>
+        <div className={`${styles.formGroup2} ${styles.formDate}`} >
+            <label  htmlFor="birthday" className={styles.formLabel}>Ngày sinh</label>
             <div> 
-                <input id="birthday" type="date" name="birthday"  className="form-input" value={values.birthday} onChange = {handleChange}/>
-            {errors.birthday &&  <span className="form-message">{errors.birthday}</span>}
+                <input id="birthday" type="date" name="birthday"  className={styles.formInput} value={values.birthday} onChange = {handleChange}/>
+            {errors.birthday &&  <span className={styles.formMessage}>{errors.birthday}</span>}
             </div>
         </div>
         </div>
             
         
-        <div className="form-group ">
-            <label  htmlFor="email" className="form-label">Email</label>
+        <div className={styles.formGroup}>
+            <label  htmlFor="email" className={styles.formLabel}>Email</label>
             <div> 
-                <input id="email" type="email" name="email" placeholder="email@hcmut.edu.com" className="form-input" value={values.email} onChange = {handleChange}/>
+                <input id="email" type="email" name="email" placeholder="email@hcmut.edu.com" className={styles.formInput} value={values.email} onChange = {handleChange}/>
             </div>
-            {errors.email &&  <span className="form-message">{errors.email}</span>}
+            {errors.email &&  <span className={styles.formMessage}>{errors.email}</span>}
         </div>
-        <div className="form-group ">
-            <label  htmlFor="phone" className="form-label">Số điện thoại</label>
+        <div className={styles.formGroup}>
+            <label  htmlFor="phone" className={styles.formLabel}>Số điện thoại</label>
             <div> 
-                <input id="phone" type="text" name="phone" placeholder="0384094123" className="form-input" value={values.phone} onChange = {handleChange}/>
+                <input id="phone" type="text" name="phone" placeholder="0384094123" className={styles.formInput} value={values.phone} onChange = {handleChange}/>
             </div>
-            {errors.phone &&  <span className="form-message">{errors.phone}</span>}
+            {errors.phone &&  <span className={styles.formMessage}>{errors.phone}</span>}
         </div>
-        <div className="form-group">
-            <label  htmlFor="password" className="form-label">Mật khẩu</label>
-            <input id="password" type="password" name="password" placeholder="Tạo mật khẩu" className="form-input" value={values.password} onChange = {handleChange}/>
-            {errors.password &&  <span className="form-message">{errors.password}</span>}
+        <div className={styles.formGroup}>
+            <label  htmlFor="password" className={styles.formLabel}>Mật khẩu</label>
+            <input id="password" type="password" name="password" placeholder="Tạo mật khẩu" className={styles.formInput} value={values.password} onChange = {handleChange}/>
+            {errors.password &&  <span className={styles.formMessage}>{errors.password}</span>}
         </div>
         
         
-        <div className="form-group">
-            {Object.keys(errors).length!==0 && <span className="form-messages">Cần nhập đẩy đủ thông tin</span>}
-        <div className="form-aside">
-            <p>Bạn đã có tài khoản?</p><Link to="/form-login" className="login-link">Đăng nhập</Link>
+        <div className={styles.formGroup}>
+            {Object.keys(errors).length!==0 && <span className={styles.formMessages}>Cần nhập đẩy đủ thông tin</span>}
+        <div className={styles.formAside}>
+            <p>Bạn đã có tài khoản?</p><Link to="/form-login" className={styles.loginLink}>Đăng nhập</Link>
         </div>
-        <button className="form-submit">Đăng ký</button>
+        <button className={styles.formSubmit}>Đăng ký</button>
         </div>
  
       </form>
